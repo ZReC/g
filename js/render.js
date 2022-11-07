a9d84f499e7a1636609a3201424d7f58dc5fb039
ZReC <17214170+ZReC@users.noreply.github.com>
2022-11-07 20:00:37 -0300

    [feature] header byte to store mime type

--- js/render.js
+++ js/render.js
@@ -1,31 +1,61 @@
 await import('./style.js');
 const link = document.createElement('a');
+let urlObject;
 
-export async function render(plaintext) {
+
+export async function render(mime, plaintext) {
     if (plaintext.byteLength < 0x8000000 && confirm('Render contents?')) {
-        await import('https://cdn.jsdelivr.net/npm/marked/marked.min.js');
+        let container, [type, subtype] = mime.split('/');
+        const tD = new TextDecoder();
 
-        const container = document.createElement('div');
-        container.style = "white-space: pre;user-select: text; font-size: 2vw;";
-        const decoded = new TextDecoder().decode(plaintext);
+        URL.revokeObjectURL(urlObject);
         try {
-            const parsed = marked.parse(decoded, { gfm: true, breaks: false });
+            switch (type) {
+                case '':
+                case 'text':
+                    switch(subtype) {
+                        case 'x-markdown':
+                        case 'markdown':
+                            await import('https://cdn.jsdelivr.net/npm/marked/marked.min.js');
 
-            try {
-                container.setHTML(parsed);
-            } catch {
-                await import('https://cdn.jsdelivr.net/npm/dompurify/purify.min.js');
-                container.innerHTML = DOMPurify.sanitize(parsed);
+                            const parsed = marked.parse(tD.decode(plaintext), { gfm: true, breaks: false });
+                            container = document.createElement('div');
+        
+                            try {
+                                container.setHTML(parsed);
+                            } catch {
+                                await import('https://cdn.jsdelivr.net/npm/dompurify/purify.min.js');
+                                container.innerHTML = DOMPurify.sanitize(parsed);
+                            }
+                            break;
+                        default:
+                            throw 'render as plain';
+                    }
+                    break;
+                case 'image':
+                    type = 'img';
+                case 'video':
+                case 'audio':
+                    container = document.createElement(type);
+                    urlObject = URL.createObjectURL(new Blob([plaintext], {type: mime}));
+                    container.controls = true;
+                    container.src = urlObject;
+                    break;
+                default:
+                    throw `can't parse mime`;
             }
-        } catch {
-            container.innerText = decoded;
+
+        } catch (e) {
+            container = document.createElement('div');
+            container.innerText = tD.decode(plaintext);
         }
 
+        container.style = "white-space: pre;user-select: text; font-size: 2vw;";
         document.body.appendChild(container);
 
     } else {
         URL.revokeObjectURL(link.href);
-        link.href = URL.createObjectURL(new Blob([plaintext], { type: 'application/octet-stream' }));
+        link.href = URL.createObjectURL(new Blob([plaintext], { type: mime }));
         link.download = 'plaintext';
         link.click();
     }
